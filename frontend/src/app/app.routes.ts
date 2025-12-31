df08ca2ab178e23c32f861b1a114dd434cf4d17a
Hossori note <law.f1.vegas@gmail.com>
2025-12-31 20:15:42 +0900

    読書記録アプリのホームコンポーネントスケルトンを作成

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'readingRecorder',
+    loadComponent: () =>
+      import('./features/reading-recorder/components/reading-recorder-home/reading-recorder-home')
+        .then(m => m.ReadingRecorderHome)
+  }
+];
---
+++ frontend/src/app/features/reading-recorder/components/reading-recorder-home/reading-recorder-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reading-recorder-home',
+  imports: [],
+  templateUrl: './reading-recorder-home.html',
+  styleUrl: './reading-recorder-home.css',
+})
+export class ReadingRecorderHome {
+
+}
